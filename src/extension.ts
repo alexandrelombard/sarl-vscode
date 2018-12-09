3890207e118aca6e78a15f88d06454d1e48fbc9c
XealRedan <alexandre.lombard@live.fr>
2018-12-09 09:53:15 +0100

    Changing launch process for the LS

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,20 @@ export function activate(context: ExtensionContext) {
      // The server is a locally installed in src/sarl
      let launcher = os.platform() === 'win32' ? 'sarl-standalone.bat' : 'sarl-standalone';
      let script = context.asAbsolutePath(path.join('src', 'sarl', 'bin', launcher));
+     let jarFile = context.asAbsolutePath(path.join('src', 'sarl', 'bin', "sarl-language-server.jar"));
+     let runJarCommand = "java -jar " + jarFile;
  
      let serverOptions: ServerOptions = {
-         run : { command: script },
-         debug: { command: script, args: [], options: { env: createDebugEnv() } }
+         run : { command: runJarCommand },
+         debug: { command: runJarCommand, args: [], options: { env: createDebugEnv() } }
      };
      
      let clientOptions: LanguageClientOptions = {
-         documentSelector: ['sarl'],
+         documentSelector: [
+             {
+                 pattern: "**/*.sarl"
+             }
+        ],
          synchronize: {
              fileEvents: workspace.createFileSystemWatcher('**/*.*')
          }
